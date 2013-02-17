51e08aa79bf57e77a573ae85ef51fe043f12dc50
Nicolai Ruckel <nicolai.ruckel@uni-weimar.de>
2013-02-17 16:41:45 +0100

    updated create_links.sh

--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ln -sf ~/dotfiles/vim ~/.vim
+echo 'ln -sf ~/dotfiles/vim ~/.vim'
 ln -sf ~/dotfiles/vimrc ~/.vimrc
+echo 'ln -sf ~/dotfiles/vimrc ~/.vimrc'
 ln -sf ~/dotfiles/vimperatorrc ~/.vimperatorrc
+echo 'ln -sf ~/dotfiles/vimperator ~/.vimperator'
 ln -sf ~/dotfiles/pentadactylrc ~/.pentadactylrc 
+echo 'ln -sf ~/dotfiles/pentadactylrc ~/.pentadactylrc'
 ln -sf ~/dotfiles/zshrc ~/.zshrc
+echo 'ln -sf ~/dotfiles/zshrc ~/.zshrc'
