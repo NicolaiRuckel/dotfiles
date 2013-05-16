5a7e6a9352739006061510a2fd0b699cc1c0a7c9
Nicolai <nicolai@ruckel.de>
2013-05-16 19:08:50 +0200

    vimsetup works

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle
-git clone git://github.com/scrooloose/syntastic.git ./vim/bundle
+git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle/nerdtree
+git clone git://github.com/scrooloose/syntastic.git ./vim/bundle/syntastic
