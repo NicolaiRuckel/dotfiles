97845d712779e32cb7d66e9cdb552f2cab2fe9fa
Nicolai Ruckel <nicolai.ruckel@uni-weimar.de>
2013-05-17 09:41:14 +0200

    moved pathogen

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle/nerdtree
 git clone git://github.com/scrooloose/syntastic.git ./vim/bundle/syntastic
+git clone git://github.com/tpope/vim-pathogen.git ./vim/bundle/vim-pathogen
