1a038f7429d289022acaa9d58180c8e471c20e74
Nicolai Ruckel <nicolai.ruckel@uni-weimar.de>
2013-05-17 09:49:59 +0200

    moved .sh files to one setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#create symlinks
 ln -s ~/dotfiles/vim ~/.vim
 echo 'ln -s ~/dotfiles/vim ~/.vim'
 ln -s ~/dotfiles/vimrc ~/.vimrc
@@ -9,3 +10,9 @@ ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc
 echo 'ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc'
 ln -s ~/dotfiles/zshrc ~/.zshrc
 echo 'ln -s ~/dotfiles/zshrc ~/.zshrc'
+#install vim plugins
+git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle/nerdtree
+git clone git://github.com/scrooloose/syntastic.git ./vim/bundle/syntastic
+git clone git://github.com/tpope/vim-pathogen.git ./vim/bundle/vim-pathogen
+#install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
--- vimsetup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle/nerdtree
-git clone git://github.com/scrooloose/syntastic.git ./vim/bundle/syntastic
-git clone git://github.com/tpope/vim-pathogen.git ./vim/bundle/vim-pathogen
