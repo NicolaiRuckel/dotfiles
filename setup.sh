03de37f319786fbfc90e2e69be00f3919657d5ac
Nicolai Ruckel <nicolai.ruckel@uni-weimar.de>
2013-06-23 18:43:21 +0200

    i3 conf

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc
 echo 'ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc'
 ln -s ~/dotfiles/zshrc ~/.zshrc
 echo 'ln -s ~/dotfiles/zshrc ~/.zshrc'
+ln -s ~/dotfiles/Xresources ~/.Xresources
+echo 'ln -s ~/dotfiles/Xresources ~/.Xresources'
+ln -s ~/dotfiles/i3status.conf ~/.i3status.conf
+echo 'ln -s ~/dotfiles/i3status.conf ~/.i3status.conf'
 #install vim plugins
 git clone git://github.com/scrooloose/nerdtree.git ./vim/bundle/nerdtree
 git clone git://github.com/scrooloose/syntastic.git ./vim/bundle/syntastic
