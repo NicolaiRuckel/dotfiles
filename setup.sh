946d6144a4b402ac26b9affff50419825192dad5
Nicolai Ruckel <nicolai.ruckel@uni-weimar.de>
2013-10-28 11:29:17 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,16 @@
 
 # for all
 ln -s ~/dotfiles/vim ~/.vim
-echo 'ln -s ~/dotfiles/vim ~/.vim'
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
-echo 'ln -s ~/dotfiles/vimrc ~/.vimrc'
 
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
-echo 'ln -s ~/dotfiles/vimrc ~/.vimrc'
 
 ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
-echo 'ln -s ~/dotfiles/vimperator ~/.vimperator'
 
 ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc 
-echo 'ln -s ~/dotfiles/pentadactylrc ~/.pentadactylrc'
 
 ln -s ~/dotfiles/zshrc ~/.zshrc
-echo 'ln -s ~/dotfiles/zshrc ~/.zshrc'
 
 # for Linux only
 OS=$(uname -s)
