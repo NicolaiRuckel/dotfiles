57a52db6259ef4a5c0d335bcdca5bc9f4c6eaa87
Nicolai Ruckel <nicolai@ruckel.de>
2015-03-13 16:48:30 +0100

    added script for i3 with sddm

---
+++ scripts/i3_startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# needed when i3 is started via sddm
+
+xset r rate 250 50
+
+#setxkbmap us altgr-intl
+setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl
+
+xmodmap -e 'clear Lock' #ensures you're not stuck in CAPS on mode
+xmodmap -e 'keycode 0x42=Escape' #remaps the keyboard so CAPS LOCK=ESC
