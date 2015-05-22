14a3449b4a72950aed385773b91cb5509dc6860f
Nicolai Ruckel <nicolai@ruckel.de>
2015-05-22 13:58:51 +0200

    new laptop \o/

--- scripts/i3_startup.sh
+++ scripts/i3_startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#  VARIABLES
+
+LAPTOP_HOSTNAME="archbook"
+DESKTOP_HOSTNAME="snitsig"
+
 # needed when i3 is started via sddm
 
 xset r rate 250 50
@@ -10,3 +15,12 @@ setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl
 
 xmodmap -e 'clear Lock' #ensures you're not stuck in CAPS on mode
 xmodmap -e 'keycode 0x42=Escape' #remaps the keyboard so CAPS LOCK=ESC
+
+case $HOSTNAME in
+    ("$LAPTOP_HOSTNAME")
+    #  laptop specific settings
+    /home/nicolai/dotfiles/scripts/laptop-touchpad-settings.sh;;
+    ("$DESKTOP_HOSTNAME")
+    #  desktop specific settings
+        ;;
+esac
