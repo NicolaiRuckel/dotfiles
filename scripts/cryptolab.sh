8b3a38d2b97722fa532dc628a26dcf83f97995b2
Nicolai Ruckel <nicolai@ruckel.de>
2017-07-15 14:15:46 +0200

    crypto setup

--- scripts/cryptolab.sh
+++ scripts/cryptolab.sh
@@ -2,11 +2,4 @@
 
 # monitor setup for media security lab
 
-modeline=$(cvt 1920 1080 60 | tail -n1 | cut -d " " -f 2-)
-modename=$(cvt 1920 1080 60 | tail -n1 | cut -d " " -f 2)
-
-xrandr --newmode $modeline
-xrandr --addmode $1 $modename
-xrandr --output DP1 --auto --output $1 --mode $modename --left-of DP1
-
-# nitrogen --restore
+xrandr --output DP1 --primary --auto --output DP2 --auto --left-of DP1 --output eDP1 --off
