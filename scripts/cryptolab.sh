beb46ed54af19a0fad8502f1b4a88c8a67dbd583
Nicolai Ruckel <nicolai@ruckel.de>
2017-12-18 17:03:30 +0100

    crypto lab script

--- scripts/cryptolab.sh
+++ scripts/cryptolab.sh
@@ -4,6 +4,7 @@
 
 LEFT_DISPLAY="--output DP1 --primary --auto"
 RIGHT_DISPLAY="--output DP2 --auto --right-of DP1"
-LAPTOP_DISPLAY="--output eDP1 --auto --left-of DP1"
+# LAPTOP_DISPLAY="--output eDP1 --auto --left-of DP1"
+LAPTOP_DISPLAY="--output eDP1 --off"
 
 xrandr $LEFT_DISPLAY $RIGHT_DISPLAY $LAPTOP_DISPLAY
