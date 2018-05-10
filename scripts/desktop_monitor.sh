cc48b713b912f0d40b4441b43131ad97ca979bc2
Nicolai Ruckel <nicolai@ruckel.de>
2018-05-10 21:01:12 +0200

    desktop_monitor script updated

--- scripts/desktop_monitor.sh
+++ scripts/desktop_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xrandr --output DP-1 --auto --primary --output HDMI-0 --auto --right-of DP-1 --rotate normal
+xrandr --output DP-1 --auto --dpi 117 --primary --output HDMI-0 --auto --right-of DP-1 --rotate normal --dpi 117
