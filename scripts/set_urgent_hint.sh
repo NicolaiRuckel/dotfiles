5bf8a3f0cafb9e21cb3c6185c2d0ec4fdf25300f
Nicolai Ruckel <nicolai@ruckel.de>
2018-05-18 11:43:09 +0200

    set_urgent_hint.sh: Evolution notifications

--- scripts/set_urgent_hint.sh
+++ scripts/set_urgent_hint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# catch Evolution notifications
+if [ "$1" == evolution-mail-notification ]; then
+  wmctrl -r Evolution -b add,demands_attention
+fi
+
 wmctrl -r $1 -b add,demands_attention
