1e93d7ae0455a3303c367b5a1b2cb0857c65a725
Nicolai Ruckel <nicolai@ruckel.de>
2018-06-06 11:25:42 +0200

    lock screen script

---
+++ scripts/lock_screen.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+scrot /tmp/lockscreenimage.png
+convert /tmp/lockscreenimage.png -scale 10% -scale 1000% /tmp/lockscreenimage.png
+xset dpms force off
+i3lock -i /tmp/lockscreenimage.png
