4c22a1366d39d057a94c591d35b9e53ae493788e
Nicolai Ruckel <nicolai@ruckel.de>
2018-07-03 13:18:20 +0200

    updated dbl script

--- scripts/dbl.sh
+++ scripts/dbl.sh
@@ -3,6 +3,6 @@
 # monitor setup for dbl
 
 RIGHT_DISPLAY="--output DP-1 --primary --auto"
-LEFT_DISPLAY="--output HDMI-1 --auto --left-of DP-1"
+LEFT_DISPLAY="--output DP-2 --auto --left-of DP-1"
 
 xrandr $LEFT_DISPLAY $RIGHT_DISPLAY
