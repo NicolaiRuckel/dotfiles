14b0891d9fcc14aec68522b336486d70aee20d2f
Nicolai Ruckel <nicolai@ruckel.de>
2018-07-04 11:53:11 +0200

    fixed lock_screen.sh

--- scripts/lock_screen.sh
+++ scripts/lock_screen.sh
@@ -4,3 +4,4 @@ scrot /tmp/lockscreenimage.png
 convert /tmp/lockscreenimage.png -scale 10% -scale 1000% /tmp/lockscreenimage.png
 xset dpms force off
 i3lock -i /tmp/lockscreenimage.png
+xset -dpms s off
