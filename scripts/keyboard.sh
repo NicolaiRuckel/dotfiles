04e304a4280216cf8a9529faee97237121f9a246
Nicolai Ruckel <nicolai@ruckel.de>
2019-03-04 16:13:29 +0100

    Map capslock to control on every machine

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -9,13 +9,8 @@ xset r rate 350 50
 # set menu to be the compose key
 setxkbmap -option compose:menu
 
-# laptop settings
-case $HOSTNAME in
-  ("archbook")
-    # map capslock to control
-    xmodmap -e 'clear lock'
-    xmodmap -e 'clear control'
-    xmodmap -e 'keycode 66 = Control_L'
-    xmodmap -e 'add control = Control_L Control_R'
-    ;;
-esac
+# map capslock to control
+xmodmap -e 'clear lock'
+xmodmap -e 'clear control'
+xmodmap -e 'keycode 66 = Control_L'
+xmodmap -e 'add control = Control_L Control_R'
