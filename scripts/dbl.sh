d3f9607376077f06e2116c9aa9f1b9d55854de1f
Nicolai Ruckel <nicolai@ruckel.de>
2020-02-12 12:55:41 +0100

    Finally turned off second display at work

--- scripts/dbl.sh
+++ scripts/dbl.sh
@@ -2,7 +2,7 @@
 
 # monitor setup for dbl
 
-RIGHT_DISPLAY="--output DP-1 --primary --auto"
-LEFT_DISPLAY="--output DP-2 --auto --left-of DP-1 --rotate normal"
+RIGHT_DISPLAY="--output DP-1 --off"
+LEFT_DISPLAY="--output DP-2 --primary --auto"
 
 xrandr $LEFT_DISPLAY $RIGHT_DISPLAY
