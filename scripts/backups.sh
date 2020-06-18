e039cc2646f57891604c1651c7d5a2353a10d310
Nicolai Ruckel <nicolai@ruckel.de>
2020-06-18 16:20:13 +0200

    Don't backup trash folder

--- scripts/backups.sh
+++ scripts/backups.sh
@@ -4,7 +4,7 @@ USER="nicolai"
 HOST="192.168.0.5"
 BACKUP_FOLDER="backups/"
 
-EXCLUDE="--exclude=.Trash* --exclude=downloads --exclude=desktop --exclude=.cache"
+EXCLUDE="--exclude=.local/share/Trash --exclude=downloads --exclude=desktop --exclude=.cache"
 
 rsync -avP --delete-excluded $EXCLUDE -e ssh ~ $USER@$HOST:$BACKUP_FOLDER
 
