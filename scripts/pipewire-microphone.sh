3374eae981f9c8ba0da19f61cba1d0618d307b49
Nicolai Ruckel <nicolai@ruckel.de>
2021-04-24 11:04:06 +0200

    Update pipewire microphone script

--- scripts/pipewire-microphone.sh
+++ scripts/pipewire-microphone.sh
@@ -4,6 +4,22 @@
 # give me a stereo signal where the right channel is silent.  This script
 # creates a new `mono-mic` source that takes the signal from the 2i2 mapped to
 # a mono channel.  Thanks to hdp from #pipewire.
+#
+# Originally I planned to start it when I boot my PC but for some reasons this
+# does not work. I tried to create a systemd service file that depends on
+# pipewire.service but that didn't help so for now I have to start it manually.
 
+# Source to be mapped to the new input device. This changed at some point and
+# therefore caused me a lot of trouble. I *think* it should end with
+# "capture_FL", so you can find it with
+#
+#    $ pw-cli list-objects | rg capture_FL
+#
+SOURCE="Scarlett 2i2 USB:capture_FL"
+
+# Create mono source
+# https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Configuration#multichannel-cards
 pactl load-module module-null-sink object.linger=1 media.class=Audio/Source/Virtual sink_name=mono-mic channel_map=mono
-pw-jack jack_connect 'Focusrite Scarlett 2i2 2nd Gen Analog Stereo:capture_FL' 'mono-mic Audio/Source/Virtual sink:input_MONO'
+
+# Link input port to source of microphone port
+pw-jack jack_connect "$SOURCE" 'mono-mic Audio/Source/Virtual sink:input_MONO'
